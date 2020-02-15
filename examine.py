9d8d88cbb2ab8d2083f2b612274bd787556b8a3c
Spandan Kumar Sahu <spandankumarsahu@gmail.com>
2020-02-16 00:37:49 +0530

    update examine.py for new strategy
    Signed-off-by: Spandan Kumar Sahu <spandankumarsahu@gmail.com>

--- examine.py
+++ examine.py
@@ -42,26 +42,22 @@ if len(sys.argv) > n:
 execstrip = executable[executable.rfind('/')+1:]
 origpath = executable[:executable.rfind('build/')-1]
 project_name = origpath[origpath.rfind('/')+1:]
-outfolder = os.path.abspath('outputs/'+project_name)
+outfolder = os.path.abspath(os.path.join('outputs', project_name))
 foutfolder = os.path.join(outfolder,'exe_'+execstrip)
 os.system('mkdir ' + foutfolder)
 
 # Extract the linking information & store in dependencies.p
 os.system('python3 parsers/project_parser.py ' + os.path.join(outfolder, 'make_log.txt') + ' ' + os.path.join(outfolder, 'dependencies.p'))
-
-# get list of all cpp's forming this executable
-def get_rec_deps(path):
-    recdeps = []
-    for x in dependencies[path]:
-        if x[-2:] == '.o':
-            recdeps.append(sourcefile[x])
-        else:
-            recdeps.extend(get_rec_deps(x))
-    return recdeps
+dependencies = pickle.load(open(os.path.join(outfolder, 'dependencies.p')))
 
 # [TODO]
 # Instead of combining by exclusive parsing, we can simply echo out to file
 # See combine_all_domain.sh, for example
+def combine(croot, droot):
+    # croot = CLANG_ROOT, droot = DWARF_ROOT
+    # important for inter-clang linkage
+    pass
+
 def combine_all_clang(depmap):
     CURFINALFILE = os.path.join(foutfolder, FINAL_FILE)
 
@@ -116,22 +112,71 @@ def combine_all_clang(depmap):
         # combine address patched xmls into one final static xml
         os.system('cat ' + exestrip + COMB_EXTENSION + ' >> ' + CURFINALFILE + STATIC_EXTENSION)
 
-def generate_static_info():
+def get_dwarf_parse(comp):
+    global foutfolder
+    compnamestrip = comp[comp.rfind('/')+1:]
+    compoutfolder = os.path.join(foutfolder, compnamestrip)
+    compstrip = os.path.join(compoutfolder, compnamestrip)
+    os.system('python3 parsers/dwxml.py '+comp+' -o '+compstrip+DWARF_EXTENSION)
+    return ET.parse(compstrip+DWARF_EXTENSION).getroot()
+
+def get_clang_info(root, comp):
     print('Starting Static!')
 
     # This function extract the dependencies of the binary under study, and
     # recursively finds out the list of source files responsible for this executable
-    # and gets the executable's DWARF information and concats them
-    ls = defaultdict(list)
-    for x in dependencies[executable]:
-        if x[-2:] == '.o':
-            ls[executable].append(sourcefile[x])
-        elif x[-2:] == '.a':
-            ls[executable].extend(get_rec_deps(x)) # .a's are also statically linked
-        elif x.find('.so') != -1:
-            ls[x] = get_rec_deps(x) # .so dynamic links treated separately
+    global dependencies
 
-    combine_all_clang(ls)
+    for x in dependencies[comp]:
+        if x[-2:] == '.a':
+            # .a's are also statically linked
+            archroot = Element('STATIC_LIBRARY')
+            archroot.set('archive_name', x)
+            generate_clang_info(archroot, x)
+            dwarfroot = get_dwarf_parse(x)
+            combine(archroot, dwarfroot)
+            root.append(archroot)
+        elif x.find('.so') != -1:
+            # For dynamic libs
+            dynroot = Element('DYNMAIC_LIBRARY')
+            dynroot.set('shared_object_name', x)
+            generate_clang_info(dynroot, x)
+            dwarfroot = get_dwarf_parse(x)
+            combine(dynroot, dwarfroot)
+            root.append(dynroot)
+        else:
+            # it is a source file
+            objroot = Element('OBJECT')
+            objroot.set('object_file', comp)
+            objroot.set('source_file', x)
+            relpath = x[len(origpath)+1:]
+            curfstrip = relpath[relpath.rfind('/')+1:relpath.rfind('.')]
+            combstrip = os.path.join(outfolder, relpath, curfstrip)
+            stree = ET.parse(combstrip + COMB_EXTENSION)
+            sroot = stree.getroot()
+            objroot.append(sroot)
+            root.append(objroot)
+
+def generate_static_info():
+    global executable
+    croot = Element('EXEC_STATIC')
+    croot.set('executable', executable)
+    generate_clang_info(croot, executable)
+    droot = get_dwarf_parse(executable)
+    combine(croot, droot)
+
+    exenamestrip = exe[exe.rfind('/')+1:]
+    exeoutfolder = os.path.join(foutfolder, exenamestrip)
+    exestrip = os.path.join(exeoutfolder, exenamestrip)
+    os.system('mkdir -p ' + exeoutfolder)
+
+    xmlstr = minidom.parseString(ET.tostring(root)).toprettyxml(indent='   ')
+    with open(exestrip+STATIC_EXTENSION, 'w') as f:
+        f.write(xmlstr)
+    print ('Written complete static clang for ' + exenamestrip)
+
+    os.system('python3 parsers/xml2ttl.py '+ exestrip+STATIC_EXTENSION)
+    print('Written complete TTL for ' + exestrip+STATIC_EXTENSION)
 
 def generate_dynamic_info(path, test=None):
     # Add dynamic_information to the combined static XML
