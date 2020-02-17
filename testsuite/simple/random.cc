2c3599b0442b8d1c0101aab8dcbace934c6344db
Kimi no nawa <vishesh0512@gmail.com>
2020-02-17 19:03:27 +0530

    Updated random testsuite

--- testsuite/simple/random.cc
+++ testsuite/simple/random.cc
@@ -1,4 +1,35 @@
-struct student;
-extern student p;
-student * u = &p;
-int f(student x);
+namespace Foo
+{
+	class A;
+	namespace Bar
+	{
+		int f(A u);
+	}
+}
+
+class Foo::A
+{
+	public:
+		int x;
+		A() {x = 0;}
+
+};
+
+namespace Foo
+{
+	int Bar::f(A v)
+	{
+		return v.x;
+	}
+}
+
+
+using namespace Foo;
+
+
+int main()
+{
+	A i;
+	int x, y = Bar::f(i);
+	return 0;
+}
