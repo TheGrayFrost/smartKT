7784e81705ad23cfcbdceabcdf32d00c8dc3d439
TheGrayFrost <vishesh0512@gmail.com>
2020-04-18 05:59:30 +0530

    testing #define's for calls.cc

--- testsuite/smallproj/main.cpp
+++ testsuite/smallproj/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include "foo.hpp"
 
+#define F f(
+#define CALLF F v
+
 unsigned long int p[][3] = {{1, 2, 3}, {4, 5, 6}};
 
 int main()
 {
 	A::m = 31;
-	A * u = f(v);
+	A * u = CALLF \
+	);
 	std::cout << "u->r = " << u->r << "\n";
 	return 0;
 }
